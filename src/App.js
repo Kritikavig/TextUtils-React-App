626bb7269ad4e34c9b5ceb828873a84440364fd5
Kritika.vig <kritikavig2003@gmail.com>
2022-05-04 01:39:08 +0530

    Added dark mode and functionality

--- src/App.js
+++ src/App.js
@@ -1,76 +1,28 @@
+import { useState } from "react";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
 
 function App() {
+  const [mode , setMode] = useState('light');
+
+  const toggleMode = ()=>{
+    if(mode==='light')
+    {
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="/">
-            Navbar
-          </a>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="/navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/">
-                  About
-                </a>
-              </li>
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  href="/"
-                  id="navbarDropdown"
-                  role="button"
-                  data-bs-toggle="dropdown"
-                  aria-expanded="false"
-                >
-                  Dropdown
-                </a>
-                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Action
-                    </a>
-                  </li>
-                  <li>
-                    <a className="dropdown-item" href="/">
-                      Another action
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              
-            </ul>
-            <form className="d-flex">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
-          </div>
-        </div>
-      </nav>
+      <Navbar title = "TextUtils" about = "About Us" mode={mode} toggleMode ={toggleMode}/>
+      <div className= "container my-3">
+      <TextForm heading = "Enter your text to analyse " mode={mode}/>
+      </div>
     </>
   );
 }
---
+++ src/components/TextForm.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const [text, setText] = useState("");
+
+  const handleClearClick = () => {
+    let newText = '';
+    setText(newText);
+  };
+
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+
+  const handleLoClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const TitleCase = () => {
+    let updatedText="";
+    let words = text.split(" ");
+    words.forEach((word) => {
+      let firstChar = word.charAt(0).toUpperCase();
+       updatedText = updatedText + (firstChar + word.slice(1))+ " ";
+    });
+    setText(updatedText);
+  };
+
+
+ const copyCase = () => {
+    let inputText = document.getElementById("myBox");
+    inputText.select();
+    navigator.clipboard.writeText(inputText.value);
+};
+
+  const handleChange = (event) => {
+    //to write in textarea
+    setText(event.target.value); //get the value
+  };
+
+  return (
+    <>
+      <div className="container" style={{color:props.mode==='dark'?'white':'black'}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+        <textarea className="form-control" style={{backgroundColor:props.mode==='dark'?'black':'white' , color:props.mode==='dark'?'white':'black'}} value={text} id="myBox" rows="8" onChange={handleChange} ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleClearClick}> Clear </button>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+        <button className="btn btn-primary mx-1" onClick={TitleCase}>Title Case</button>
+        <button className="btn btn-primary mx-1" onClick={copyCase}>Copy Text</button>
+      </div>
+
+      <div className="container my-4" style={{color:props.mode==='dark'?'white':'black'}}>
+        <h2>Your text summary :</h2>
+        <p>{text.split(" ").length-1} words and {text.length} characters</p>
+        {/* time taken to read words */}
+        <p>{0.008 * text.split(" ").length} minutes read </p>   
+      </div>
+    </>
+  );
+}
